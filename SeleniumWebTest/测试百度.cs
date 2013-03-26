35a987948381f2fb38198954622935c0af888a40
赵晏龙 <ZhaoYanLong@skight.com>
2013-03-26 22:49:49 +0800

    将“测试百度”改为MSpecs的语法

--- SeleniumWebTest/测试百度.cs
+++ SeleniumWebTest/测试百度.cs
@@ -1,51 +1,53 @@
 ﻿using System;
 using System.Text;
+using Machine.Specifications;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 
 namespace SeleniumWebTest
 {
-    [TestFixture]
     public class 测试百度
     {
-        private IWebDriver driver;
-        private StringBuilder verificationErrors;
-        private string baseURL;
-        private bool acceptNextAlert = true;
+        private static IWebDriver driver;
+        private static StringBuilder verification_errors;
+        private static string base_url;
+        private static bool accept_next_alert = true;
 
-        [SetUp]
-        public void SetupTest()
-        {
-            driver = new FirefoxDriver();
-            baseURL = "http://www.baidu.com";
-            verificationErrors = new StringBuilder();
-        }
-
-        [TearDown]
-        public void TeardownTest()
-        {
-            try
+        private Establish that =
+            () =>
             {
-                driver.Quit();
-            }
-            catch (Exception)
+                driver = new FirefoxDriver();
+                base_url = "http://www.baidu.com";
+                verification_errors = new StringBuilder();
+
+                driver.Navigate().GoToUrl(base_url + "/");
+                driver.FindElement(By.Id("kw")).Clear();
+                driver.FindElement(By.Id("kw")).SendKeys("暴走漫画");
+                driver.FindElement(By.Id("su")).Click();
+            };
+
+        private Cleanup after_each =
+            () =>
             {
-                // Ignore errors if unable to close the browser
-            }
-            Assert.AreEqual("", verificationErrors.ToString());
-        }
+                try
+                {
+                    driver.Quit();
+                }
+                catch (Exception)
+                {
+                    // Ignore errors if unable to close the browser
+                }
+                verification_errors.ToString().ShouldEqual("");
+            };
 
-        [Test]
-        public void TheUntitledTest()
-        {
-            driver.Navigate().GoToUrl(baseURL + "/");
-            driver.FindElement(By.Id("kw")).Clear();
-            driver.FindElement(By.Id("kw")).SendKeys("暴走漫画");
-            driver.FindElement(By.Id("su")).Click();
-        }
+        private It should_do_something =
+            () =>
+            {
+                
+            };
 
-        private bool IsElementPresent(By by)
+        private static bool is_element_present(By by)
         {
             try
             {
@@ -58,12 +60,12 @@ namespace SeleniumWebTest
             }
         }
 
-        private string CloseAlertAndGetItsText()
+        private static string close_alert_and_get_its_text()
         {
             try
             {
                 IAlert alert = driver.SwitchTo().Alert();
-                if (acceptNextAlert)
+                if (accept_next_alert)
                 {
                     alert.Accept();
                 }
@@ -75,7 +77,7 @@ namespace SeleniumWebTest
             }
             finally
             {
-                acceptNextAlert = true;
+                accept_next_alert = true;
             }
         }
     }
