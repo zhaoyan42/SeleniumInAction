07fe58f96fe1a4c7fc9b5d071b467f0dc1b8cc31
赵晏龙 <ZhaoYanLong@skight.com>
2013-03-28 00:18:13 +0800

    TargetMvcApplication加入清空数据库的Controller

---
+++ TargetMvcApplication/Controllers/ClearDatabaseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TargetMvcApplication.Context;
+
+namespace TargetMvcApplication.Controllers
+{
+    public class ClearDatabaseController : Controller
+    {
+        //
+        // GET: /ClearDatabase/
+
+        public ActionResult Index()
+        {
+            Database.Delete("TargetMvcApplicationContext");
+            var context = new TargetMvcApplicationContext();
+            context.Database.Initialize(true);
+            return View();
+        }
+
+    }
+}
